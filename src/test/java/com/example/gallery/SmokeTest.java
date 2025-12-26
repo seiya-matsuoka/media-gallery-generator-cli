3ded01dcd2bcd59c21ab747af1138b583c799182
seiya matsuoka <seiya.matsuoka0413@gmail.com>
2025-12-26 21:10:54 +0900

    test: スモークテストを作成
    - JUnitの動作確認用テストを作成

---
+++ src/test/java/com/example/gallery/SmokeTest.java
@@ -0,0 +1,12 @@
+package com.example.gallery;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class SmokeTest {
+  @Test
+  void itWorks() {
+    assertTrue(true);
+  }
+}
